80fadec53fbceb41634dae34d759af4849568291
Your Name <you@example.com>
2023-02-24 19:39:02 -0800

    start work on js

--- js/index.js
+++ js/index.js
@@ -2,4 +2,25 @@
 function button(element, color) {
     element.style.background = color;
 }
-button()
+button();
+
+// fom section 
+function form() {
+    let username = document.getElementById("name").value;
+    let emailuser = document.getElementById("email").value;
+    let subject = document.getElementById("sub").value;
+    let vaild = true;
+    if (username == "") {
+        alert("write your name");
+        vaild = false;
+    }
+    if (emailuser == "") {
+        alert("write your Email");
+        vaild = false;
+    }
+    if (subject == "") {
+        alert("write your subject");
+        vaild = false;
+    }
+    return vaild;
+}
